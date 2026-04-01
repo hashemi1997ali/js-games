70485b47cc1c40aea9584e2a5add8ae711ea4bd4
Ali Hashemi <hashemi1997ali@gmail.com>
2026-04-01 03:54:30 +0200

    add pl game

---
+++ pig-latin.js
@@ -0,0 +1,26 @@
+const vowels = ["a", "e", "i", "o", "u"];
+const playerSentence = process.argv[2];
+
+function translateToPigLatin(sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => {
+      if (vowels.includes(word[0])) {
+        return word + "way";
+      } else {
+        const firstVowelIndex = word
+          .split("")
+          .findIndex((char) => vowels.includes(char));
+        if (firstVowelIndex !== -1) {
+          const consonantCluster = word.slice(0, firstVowelIndex);
+          const restOfWord = word.slice(firstVowelIndex);
+          return restOfWord + consonantCluster + "ay";
+        } else {
+          return word + "ay";
+        }
+      }
+    })
+    .join(" ");
+}
+
+console.log(translateToPigLatin(playerSentence));
